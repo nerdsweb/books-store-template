bc27bf88be7bb06a06824d8e40e8f57531a4cb96
gcarreno <gcarreno@pagochinchin.com>
2024-05-02 08:50:30 -0400

    creados módulos principales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export const CC_PROJECT_INITIALS = 'makeup-store-template';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'books-store-template';
+  title = 'meat-store-template';
+
+  ngOnInit() {
+
+    document.documentElement.style.setProperty('--v-dynamic-primary', '#260719');
+    document.documentElement.style.setProperty('--v-dynamic-secondary', '#C90077');
+    document.documentElement.style.setProperty('--v-dynamic-tertiary', '#801660');
+
+  }
+
 }
+

8b68fb863d7f3dd47140de163decd36793ab9865
GabrielC-Web <gabrieljose.cz21@gmail.com>
2024-05-04 08:14:01 -0400

    Auto-generated commit [ci skip]

---
+++ assets/images/image-routes.ts
@@ -0,0 +1,40 @@
+/**
+ * Íconos generales
+ */
+export const icons = {
+    van: 'assets/images/icons/van.svg',
+    twitter: 'assets/images/icons/icons8-twitter.svg',
+    facebook: 'assets/images/icons/icons8-facebook.svg',
+    instagram: 'assets/images/icons/icons8-instagram.svg',
+    whatsapp: 'assets/images/icons/whatsapp.svg',
+}
+
+export const logos = {
+    daves: 'assets/images/logos/daves.svg',
+}
+
+export const images = {
+    product1: 'assets/images/products/product1.jpg',
+    product2: 'assets/images/products/product2.jpg',
+    product3: 'assets/images/products/product3.jpg',
+    product4: 'assets/images/products/product4.jpg',
+    product5: 'assets/images/products/product5.jpg',
+    product6: 'assets/images/products/product6.jpg',
+    product7: 'assets/images/products/product7.jpg',
+    product8: 'assets/images/products/product8.jpg',
+    product9: 'assets/images/products/product9.jpg',
+    product10: 'assets/images/products/product10.jpg',
+    product11: 'assets/images/products/product11.jpg',
+    product12: 'assets/images/products/product12.jpg',
+    product13: 'assets/images/products/product13.jpg',
+    product14: 'assets/images/products/product14.jpg',
+    product15: 'assets/images/products/product15.jpg',
+    product16: 'assets/images/products/product16.jpg',
+    product17: 'assets/images/products/product17.jpg',
+    product18: 'assets/images/products/product18.jpg',
+    product19: 'assets/images/products/product19.jpg',
+    product20: 'assets/images/products/product20.jpg',
+    product21: 'assets/images/products/product21.jpg',
+    product22: 'assets/images/products/product22.jpg',
+    home_bg: 'assets/images/home_bg.webp'
+}

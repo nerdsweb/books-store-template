4c78e1ad1ec4c088cfa014fea7c00fe9bb22bdfe
GabrielC-Web <gabrieljose.cz21@gmail.com>
2024-05-05 20:07:07 -0400

    Terminadas vistas de la plantilla

--- src/app/modules/products/pages/products-detail-layout/products-detail-layout.component.ts
+++ src/app/modules/products/pages/products-detail-layout/products-detail-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { CmmEcomProductDetailModelv2 } from 'src/app/common/data/utils/models/ecommerce.models';
 import { icons, images } from 'src/assets/images/image-routes';
 
@@ -77,10 +78,52 @@ export class ProductsDetailLayoutComponent {
     ],
   }
 
+  products: any[] = [
+    {
+      image: images.product5,
+      title: 'PADRE RICO PADRE POBRE',
+      brand: 'Robert T. Kiyosaki',
+      price: '14,00',
+      previousprice: '27,00',
+      currency: '$',
+      id: '5'
+    },
+    {
+      image: images.product6,
+      title: 'UN CUENTO PERFECTO',
+      brand: 'ELÍSABET BENAVENT',
+      price: '14,00',
+      previousprice: '27,00',
+      currency: '$',
+      id: '6'
+    },
+    {
+      image: images.product7,
+      title: 'HÁBITOS ATÓMICOS',
+      brand: 'JAMES CLEAR',
+      price: '14,00',
+      previousprice: '27,00',
+      currency: '$',
+      id: '7'
+    },
+  ]
+
   promos = []
 
   items: any[] = []
 
   smallMode: boolean = false
+
+  constructor(
+    private router: Router
+  ) { }
+
+  /**
+   * Me lleva a la vista de detalle
+   * @param id 
+   */
+  navigate(id: string) {
+    this.router.navigate(['/products/detail'])
+  }
 
 }
--- src/app/modules/products/pages/products-overview-layout/products-overview-layout.component.ts
+++ src/app/modules/products/pages/products-overview-layout/products-overview-layout.component.ts
@@ -8,7 +8,49 @@ import { images } from 'src/assets/images/image-routes';
 })
 export class ProductsOverviewLayoutComponent {
 
-  productsList: any[] = []
+  productsList: any[] = [
+    {
+      image: images.product5,
+      title: 'PADRE RICO PADRE POBRE',
+      brandName: 'Robert T. Kiyosaki',
+      sku: 'krogm93746917',
+      price: '14,00',
+      previousprice: '27,00',
+      quantity: 1,
+      currency: '$',
+      id: '5'
+    },
+    {
+      images: [
+        images.product20,
+      ],
+      image: images.product6,
+      name: 'Never split the difference',
+      brandName: 'Chris Voss',
+      sku: 'krogm93746917',
+      price: '175.00',
+      quantity: 1,
+      aditionalInfo: [
+        {
+          title: 'sinopsis',
+          texts: [
+            'El prestigioso ex negociador internacional del FBI Chris Voss, especializado en secuestros con rehenes, nos enseña un método de negociación rompedor: tácticas para negociaciones duras que son aplicables en múltiples aspectos de nuestras vidas.',
+          ]
+        },
+      ],
+    },
+    {
+      image: images.product7,
+      title: 'HÁBITOS ATÓMICOS',
+      brandName: 'JAMES CLEAR',
+      price: '14,00',
+      quantity: 1,
+      previousprice: '27,00',
+      currency: '$',
+      id: '7'
+    },
+  ]
+
   prebill: any = {
     basePrice: 175,
     delivery: 175,
